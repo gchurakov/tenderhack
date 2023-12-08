eceb18c78b5e4e7fbc4bba6eac33b93f49d73171
ilya notebook <ressivage@ya.ru>
2023-12-08 22:03:54 +0500

    flask db

---
+++ back/main.py
@@ -0,0 +1,14 @@
+from db import tables, engines, inspectors
+from flask import Flask
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+app = Flask(__name__)
+
+table = tables['common']['my_table']
+
+@app.route("/")
+def hello_world():
+    with Session(engines['common']):
+        return {'result':[r._mapping for r in select(table.c).select_from(table).all()]}
+    return "<p>Hello, World!</p>"
