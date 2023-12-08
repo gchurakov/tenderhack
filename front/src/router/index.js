3e3d64ace86b5ae95c26e64e5f504cb9a5682e2f
Виталий Тютюников <vtyutyunikov@ekonomexpert.ru>
2023-12-09 02:46:19 +0500

    #styles add tenderlist and tenderitem

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,16 +2,12 @@
 // import Posts from "../pages/Posts";
 // import PostIdPage from "../pages/PostIdPage";
 // import Login from "../pages/Login";
-import MainPage from "../pages/MainPage"
-import StartPage from "../pages/StartPage"
+import MainPage from '../components/pages/MainPage';
 
 export const privateRoutes = [
     // {path: '/about', component: About, exact: true},
     // {path: '/posts', component: Posts, exact: true},
     // {path: '/posts/:id', component: PostIdPage, exact: true},
-]
+];
 
-export const publicRoutes = [
-    {path: '/', component: StartPage, exact: true},
-    {path: '/main', component: MainPage, exact: true},
-]
+export const publicRoutes = [{ path: '/', component: MainPage, exact: true }];
