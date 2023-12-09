35fbfd88971ada2cf60aeeb3e1aec7cbfe4c7870
ilya notebook <ressivage@ya.ru>
2023-12-09 15:22:09 +0500

    last commit

--- front/src/components/UI/messages/ArbitraryItemMessage/ArbitraryItemMessage.jsx
+++ front/src/components/UI/messages/ArbitraryItemMessage/ArbitraryItemMessage.jsx
@@ -7,7 +7,7 @@ import { Link, Button } from '@mui/material';
 import { TextField } from '@mui/material';
 
 function ArbitraryItemMessage() {
-    const data = { 'documentId':'834954', 'item':1, 'proposedEdition':'поставить 80 кальянов и 10 чарон ьейьи', 'comment':'бля' }
+    const data = { 'documentId': '834954', 'item': 3, 'proposedEdition': 'отменить поставку 10 стульев', 'comment': 'мы не можем себе это позволить' }
     return (
 
         <Card variant="outlined">
@@ -19,8 +19,6 @@ function ArbitraryItemMessage() {
                 <Typography>Пункт: {data.item}</Typography>
                 <Typography>Предложенная редакция: "{data.proposedEdition}"</Typography>
                 <Typography>Комментарий: "{data.comment}"</Typography>
-                
-
 
             </CardContent>
         </Card>
