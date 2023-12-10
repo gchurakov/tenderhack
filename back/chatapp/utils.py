99ca9df43642d420e8b97f0741457720a6e38291
Michael <68405115+Mishania12let@users.noreply.github.com>
2023-12-10 05:50:43 +0500

    login and events

--- back/chatapp/utils.py
+++ back/chatapp/utils.py
@@ -1,4 +1,7 @@
-from .models.core import Tender, Document, ContractClause
+from passlib.handlers.sha2_crypt import sha256_crypt
+from datetime import datetime
+
+from .models.core import Tender, Document, User, Message
 from .db import engine, db_session, Base
 
 
@@ -8,25 +11,43 @@ def create_dummy_objects():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
-    tender = Tender(description="wow", tender_number=123)
+    tender = Tender(description="wow", tender_number="№564241")
     db_session.add(tender)
     db_session.commit()
 
-    obj = Document(document_name="TestDoc", tender_id=1, document_type=2)
-    db_session.add(obj)
-    db_session.commit()
-
-    obj = Document(document_name="TestDoc", tender_id=1, document_type=2)
-    db_session.add(obj)
-    db_session.commit()
-
-    obj = ContractClause(
-        document_id=1,
-        tender_id=1,
-        before_clause="Before text",
-        clause="Now clause",
-        comment="Ha ha"
+    obj = User(
+        username="user1",
+        password=sha256_crypt.hash("root"),
+        name="test1"
     )
+    db_session.add(obj)
+    db_session.commit()
 
+    obj = User(
+        username="user2",
+        password=sha256_crypt.hash("root"),
+        name="test2"
+    )
+    db_session.add(obj)
+    db_session.commit()
+
+    obj = Message(
+        data="{}",
+        decision=0,
+        type="contract_protocol",
+        time_iso=datetime.now().isoformat(),
+        tender_id=1,
+        user_id=1
+    )
+    db_session.add(obj)
+    db_session.commit()
+
+    obj = Document(
+        name="{}",
+        tender_id=1,
+        document_uri="contract_protocol",
+        document_binary_data=datetime.now().isoformat(),
+        document_binary_data_ext=1
+    )
     db_session.add(obj)
     db_session.commit()
